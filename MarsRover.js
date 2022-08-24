849df3d8a6d43c61309bcf00f51a0b72ddd05f2a
abdoshawky1 <58951633+abdoshawky1@users.noreply.github.com>
2022-08-24 03:36:13 +0200

    Update MarsRover.js

--- MarsRover.js
+++ MarsRover.js
@@ -1,145 +1,147 @@
-function MarsRover(location, direction, grid, obstacles) {
-self = this;
-        
-        this.location = (location === undefined) ? [0, 0] : location;
-        this.direction = (direction === undefined) ? 'N' : direction;
-        this.grid = (grid === undefined) ? [100, 100] : grid;
-        this.obstacles = (obstacles === undefined) ? [] : obstacles;
-       
-        this.status = 'OK';
+    class MarsRover {
+         
+        constructor(location, direction, grid, obstacles) {
+ 
+            this.location = (location === undefined) ? [0, 0] : location;
+            this.direction = (direction === undefined) ? 'N' : direction;
+            this.grid = (grid === undefined) ? [100, 100] : grid;
+            this.obstacles = (obstacles === undefined) ? [] : obstacles;
 
-        this.commands = function (commands) {
-            if (commands === undefined) { // Getter
-                return this.commandsArray;
-            } else { // Setter
-                for (var index = 0; index < commands.length; index++) {
-                    var command = commands[index];
-                    if (command === 'f' || command === 'b') {
-                        if (!move(command))
-                            break;
-                    } else if (command === 'l' || command === 'r') {
-                        turn(command);
+            this.status = 'OK';
+        }
+            commands (commands) {
+                if (commands === undefined) { // Getter
+                    return this.commandsArray;
+                } else { // Setter
+                    for (var index = 0; index < commands.length; index++) {
+                        var command = commands[index];
+                        if (command === 'f' || command === 'b') {
+                            if (!this.move(command))
+                                break;
+                        } else if (command === 'l' || command === 'r') {
+                            this.turn(command);
+                        }
+                    }
+                    this.resetLocation();
+                    this.commandsArray = commands;
+                }
+            }
+
+             resetLocation() {
+                this.location = [
+                    (this.location[0] + this.grid[0]) % this.grid[0],
+                    (this.location[1] + this.grid[1]) % this.grid[1],
+                ];
+            }
+
+             move(command) {
+                var xIncrease = 0, yIncrease = 0;
+
+                this.direction === 'N' ? yIncrease = -1 : 0;
+                this.direction === 'E' ? xIncrease = 1 : 0;
+                this.direction === 'S' ? yIncrease = 1 : 0;
+                this.direction === 'W' ? xIncrease = -1 : 0;
+
+
+
+
+                if (command === 'b') { // Backward
+                    xIncrease *= -1;
+                    yIncrease *= -1;
+                }
+
+                var newLocation = [this.location[0] + xIncrease, this.location[1] + yIncrease];
+                if (this.isObstacle(newLocation)) {
+                    return false;
+                }
+                this.location = newLocation;
+
+                return true;
+
+            }
+             isObstacle(newLocation) {
+                for (var index = 0; index < this.obstacles.length; index++) {
+                    if (newLocation.toString() == this.obstacles[index].toString()) {
+                        this.status = 'obstacle';
+                        return true;
                     }
                 }
-                resetLocation();
-                this.commandsArray = commands;
+                return false;
             }
-        };
+            turn(command) {
+                var newDirection;
+                switch (this.direction) {
+                    case 'N':
+                        if (command === 'r') {
+                            this.direction = 'E';
+                            newDirection = this.direction;
+                        }
+                        else if (command === 'l') {
+                            this.direction = 'W';
+                            newDirection = this.direction;
 
-        function resetLocation() {
-            self.location = [
-                (self.location[0] + self.grid[0]) % self.grid[0],
-                (self.location[1] + self.grid[1]) % self.grid[1],
-            ];
-        }
+                        }
+                        break;
 
-        function move(command) {
-           var xIncrease = 0, yIncrease = 0;
+                    case 'S':
+                        if (command === 'r') {
+                            this.direction = 'W';
+                            newDirection = this.direction;
 
-            self.direction === 'N' ? yIncrease = -1 : 0;
-            self.direction === 'E' ? xIncrease = 1 : 0;
-            self.direction === 'S' ? yIncrease = 1 : 0;
-            self.direction === 'W' ? xIncrease = -1 : 0;
-         
+                        }
+                        else if (command === 'l') {
+                            this.direction = 'E';
+                            newDirection = this.direction;
+
+                        }
+                        break;
+
+                    case 'E':
+                        if (command === 'r') {
+                            this.direction = 'S';
+                            newDirection = this.direction;
+
+                        }
+                        else if (command === 'l') {
+                            this.direction = 'N';
+                            newDirection = this.direction;
+
+                        }
+                        break;
+
+                    case 'W':
+                        if (command === 'r') {
+                            this.direction = 'N';
+                            newDirection = this.direction;
+
+                        }
+                        else if (command === 'l') {
+                            this.direction = 'S';
+                            newDirection = this.direction;
+
+                        }
+                        break;
+                }
+
+
+                this.direction = newDirection;
+
+                return true;
+            }
 
 
         
-        if (command === 'b') { // Backward
-            xIncrease *= -1;
-            yIncrease *= -1;
         }
 
-        var newLocation = [self.location[0] + xIncrease, self.location[1] + yIncrease];
-        if (isObstacle(newLocation)) {
-            return false;
-        }
-        self.location = newLocation;
 
-        return true;
-
-        }
-         function isObstacle(newLocation) {
-            for(var index = 0; index < this.obstacles.length; index++) {
-                if (newLocation.toString() == self.obstacles[index].toString()) {
-                    self.status = 'obstacle';
-                    return true;
-                }
-            }
-            return false;
-        }
-        function turn(command) {
-            var newDirection;
-            switch(self.direction){
-            case 'N':
-            if(command==='r') {
-                self.direction ='E';
-                newDirection = self.direction;
-            }
-            else if(command==='l'){
-                self.direction ='W';
-                newDirection = self.direction;
-
-            }
-            break;
-            
-            case 'S':
-            if(command==='r') {
-                self.direction ='W';
-                newDirection = self.direction;
-
-            }
-            else if(command==='l'){
-                self.direction ='E';
-                newDirection = self.direction;
-
-            }
-            break;
-            
-            case 'E':
-            if(command==='r') {
-                self.direction ='S';
-                newDirection = self.direction;
-
-            }
-            else if(command==='l'){
-                self.direction ='N';
-                newDirection = self.direction;
-
-            }
-            break;
-            
-            case 'W':
-            if(command==='r') {
-                self.direction ='N';
-                newDirection = self.direction;
-
-            }
-            else if(command==='l'){
-                self.direction ='S';
-                newDirection = self.direction;
-
-            }
-            break;
-            }
-
-           
-            self.direction = newDirection ;
-            
-            return true;
-                }
+    
 
 
 
-
-                
-
-    }
+ var rover = new MarsRover([12,21],"E");
+rover.commands(["l"]);
 
 
 
-let rover = new MarsRover([12,21],"N");
-rover.commands(["F"],["F"]);
-
-
-
-console.log(rover.location);
+console.log(rover.direction)
+    
